8b85da1290ad7f74388d53067f08571055b00d7c
Tavo Cruz <madecruz2@gmail.com>
2024-03-06 22:44:13 -0800

    convert svgs to components

---
+++ frontend/src/svgs/RepostSVG.js
@@ -0,0 +1,7 @@
+import { ReactComponent as RepostIconSVG } from "../../public/repost.svg";
+
+const RepostSVG = (props) => {
+  return <RepostIconSVG {...props} />
+}
+
+export default RepostSVG;
---
+++ frontend/src/svgs/ShareSVG.js
@@ -0,0 +1,7 @@
+import { ReactComponent as ShareIconSVG } from "../../public/share.svg";
+
+const ShareSVG = (props) => {
+  return <ShareIconSVG {...props} />
+}
+
+export default ShareSVG;
